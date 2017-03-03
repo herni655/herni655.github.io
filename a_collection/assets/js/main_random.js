68c2598a41c8953dd9fa37f7b6194a1bd01fc266
Irene Hernandez <herni655@newschool.edu>
2017-03-03 10:31:16 -0500

    week 6

--- a_collection/assets/js/main_random.js
+++ a_collection/assets/js/main_random.js
@@ -1,7 +1,7 @@
 var sources = [
-"../media/Healthy/Vegetables/baby_carrot.jpg",
-"../media/Healthy/Vegetables/Bell-Pepper.jpg",
-"../media/Healthy/Vegetables/celery.jpg"
+"./assets/media/Healthy/Vegetables/baby_carrot.jpg",
+"./assets/media/Healthy/Vegetables/Bell-Pepper.jpg",
+"./assets/media/Healthy/Vegetables/celery.jpg"
 ];
 
 var imgObject = document.getElementById('random-image');
@@ -9,6 +9,7 @@ var imgObject = document.getElementById('random-image');
 var randomNum = Math.floor(Math.random() * sources.length)
 
 imgObject.src = sources[randomNum];
+
 
 // var mySources = ["../media/Healthy/Vegetables/baby_carrot.jpg",
 //  "../media/Healthy/Vegetables/Bell-Pepper.jpg",
@@ -18,6 +19,24 @@ imgObject.src = sources[randomNum];
 //  	var random = Math.random() * 3;
 //  	random = Math.floor(random);
 
-//  	document.getElementById("image").inner.HTML;
+//  	document.write("<img scr = /"" + Math.floor(1 + Math.random()3) + .jpg\" />");
 //  }
+
+// function randomImg() {
+// 	var myImages = new Array();
+
+// 	myImages[1] = "../media/Healthy/Vegetables/baby_carrot.jpg";
+// 	myImages[2] = "../media/Healthy/Vegetables/Bell-Pepper.jpg";
+// 	myImages[3] = "../media/Healthy/Vegetables/celery.jpg";
+
+// 	// var myLinks = new Array();
+// 	// myLinks[1]
+// }
+
+// var rnd = Math.floor(Math.random() * myImages.length);
+// if(rnd == 0) {
+// 	rnd =  1;
+// }
+
+// document.write(document.getElementById("randomImg") + (img scr = myImages[rnd]+ '"border = "0"');
 
