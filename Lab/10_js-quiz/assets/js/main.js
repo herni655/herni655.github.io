0bed117c18bc1dd953b306725366a4a04f9e9729
Irene Hernandez <herni655@newschool.edu>
2017-04-06 09:35:09 -0400

    quiz

---
+++ Lab/10_js-quiz/assets/js/main.js
@@ -0,0 +1,116 @@
+function checkerboard(width, height) {
+
+var checkerboard = "";
+
+for (j = 0; j < height; j++){
+   if (j % 2) {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + "#"
+           } else {
+               checkerboard = checkerboard + " "
+           }
+       }
+   } else {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + " "
+           } else {
+               checkerboard = checkerboard + "#"
+           }
+       }
+   }
+   checkerboard = checkerboard + "\n"
+}
+
+console.log(checkerboard);
+}
+
+
+
+function checkerboard(width, height) {
+
+var checkerboard = "";
+
+for (j = 0; j < height; j++){
+   if (j % 2) {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + "#"
+           } else {
+               checkerboard = checkerboard + " "
+           }
+       }
+   } else {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + " "
+           } else {
+               checkerboard = checkerboard + "#"
+           }
+       }
+   }
+   checkerboard = checkerboard + "\n"
+}
+
+console.log(checkerboard);
+}
+
+
+function checkerboard(width, height) {
+
+var checkerboard = "";
+
+for (j = 0; j < height; j++){
+   if (j % 2) {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + "#"
+           } else {
+               checkerboard = checkerboard + " "
+           }
+       }
+   } else {
+       for (i = 0; i < width; i++) {
+           if ( i % 2 ){
+               checkerboard = checkerboard + " "
+           } else {
+               checkerboard = checkerboard + "#"
+           }
+       }
+   }
+   checkerboard = checkerboard + "\n"
+}
+
+console.log(checkerboard);
+}
+	
+
+function triangle(height) {
+	var pyramid = "#";
+	for (var i = 0; i < height; i++) {
+		[i]
+	}
+}
+
+
+
+
+
+
+//buttons
+
+var checkerboardBtn = document.getElementById("checkerboard");
+checkerboardBtn.addEventListener("click", function(event) { 
+	console.log("checkerboard(width(5), height)(5)")
+});
+
+var pyramidBtn = document.getElementById("pyramid")
+pyramidBtn.addEventListener("click", function(event) { 
+	
+});
+
+
+
+
+
