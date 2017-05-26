bc723f26a8f90b054e82ff1ea6f6f79c3947f2af
Irene Hernandez <herni655@newschool.edu>
2017-05-26 16:21:48 -0400

    portfolio

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,36 +1,102 @@
-$(document).ready(function() {
+// target to give background to
+var $div = document.getElementById("gradient");
+// rgb vals of the gradients
+var gradients = [
+  { start: [149,56,70], stop: [85,65,107] },
+  { start: [43,72,74], stop: [149,56,70] },
+  { start: [85,65,107], stop: [85,65,107] }
+];
+// how long for each transition
+var transition_time = 4;
 
-	$('.image-popup-vertical-fit').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-		
-	});
+// internal type vars
+var currentIndex = 0; // where we are in the gradients array
+var nextIndex = 1; // what index of the gradients array is next
+var steps_count = 0; // steps counter
+var steps_total = Math.round(transition_time*60); // total amount of steps
+var rgb_steps = {
+  start: [0,0,0],
+  stop: [0,0,0]
+}; // how much to alter each rgb value
+var rgb_values = {
+  start: [0,0,0],
+  stop: [0,0,0]
+}; // the current rgb values, gets altered by rgb steps on each interval
+var prefixes = ["-webkit-","-moz-","-o-","-ms-",""]; // for looping through adding styles
+var div_style = $div.style; // short cut to actually adding styles
+var color1, color2;
 
-	$('.image-popup-fit-width').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
-		}
-	});
+// sets next current and next index of gradients array
+function set_next(num) {
+  return (num + 1 < gradients.length) ? num + 1 : 0;
+}
 
-	$('.image-popup-no-margins').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
-		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
-		}
-	});
+// work out how big each rgb step is
+function calc_step_size(a,b) {
+  return (a - b) / steps_total;
+}
 
-});
+// populate the rgb_values and rgb_steps objects
+function calc_steps() {
+  for (var key in rgb_values) {
+    if (rgb_values.hasOwnProperty(key)) {
+      for(var i = 0; i < 3; i++) {
+        rgb_values[key][i] = gradients[currentIndex][key][i];
+        rgb_steps[key][i] = calc_step_size(gradients[nextIndex][key][i],rgb_values[key][i]);
+      }
+    }
+  }
+}
+
+// update current rgb vals, update DOM element with new CSS background
+function updateGradient(){
+  // update the current rgb vals
+  for (var key in rgb_values) {
+    if (rgb_values.hasOwnProperty(key)) {
+      for(var i = 0; i < 3; i++) {
+        rgb_values[key][i] += rgb_steps[key][i];
+      }
+    }
+  }
+
+  // generate CSS rgb values
+  var t_color1 = "rgb("+(rgb_values.start[0] | 0)+","+(rgb_values.start[1] | 0)+","+(rgb_values.start[2] | 0)+")";
+  var t_color2 = "rgb("+(rgb_values.stop[0] | 0)+","+(rgb_values.stop[1] | 0)+","+(rgb_values.stop[2] | 0)+")";
+
+  // has anything changed on this interation
+  if (t_color1 != color1 || t_color2 != color2) {
+
+    // update cols strings
+    color1 = t_color1;
+    color2 = t_color2;
+
+    // update DOM element style attribute
+    div_style.backgroundImage = "-webkit-gradient(linear, left bottom, right top, from("+color1+"), to("+color2+"))";
+    for (var i = 0; i < 4; i++) {
+      div_style.backgroundImage = prefixes[i]+"linear-gradient(45deg, "+color1+", "+color2+")";
+    }
+  }
+
+  // we did another step
+  steps_count++;
+  // did we do too many steps?
+  if (steps_count > steps_total) {
+    // reset steps count
+    steps_count = 0;
+    // set new indexs
+    currentIndex = set_next(currentIndex);
+    nextIndex = set_next(nextIndex);
+    // calc steps
+    calc_steps();
+  }
+
+  if (div_style.backgroundImage.indexOf("gradient") != -1) {
+    window.requestAnimationFrame(updateGradient)
+  }
+}
+
+// initial step calc
+calc_steps();
+
+// go go go!
+window.requestAnimationFrame(updateGradient);
